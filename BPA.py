89cb810c73cde3116fc9c0cbb6c686c3fb908124
Jholman Daniel Reina Toledo <reina.daniel1005@gmail.com>
2022-03-06 09:51:01 -0500

    ya funciona BPA

--- BPA.py
+++ BPA.py
@@ -6,8 +6,8 @@ import general
 class algoritmo_BPA:
 
     def __init__(self, puzzle_inicial, puzzle_final):
-        self.inicial = nodo_class(None, puzzle_inicial, "")
-        self.fin = puzzle_final
+        self.inicial = nodo_class(None, puzzle_inicial.copy(), "")
+        self.fin = puzzle_final.copy()
         self.lista_abierto = [self.inicial]
         self.lista_cerrado = []
         self.iniciar()
@@ -15,57 +15,67 @@ class algoritmo_BPA:
     def iniciar(self):
         contador = 0
         while len(self.lista_abierto) > 0:
-            print(contador)
+            #print("primera", contador)
             contador = contador + 1
             nodo = self.lista_abierto[0]
+            print(len(self.lista_cerrado))
             self.lista_abierto.pop(0)
-            if not general.nodo_en_lista(self.lista_cerrado, nodo): #Verificar toda la rama hacia el padre que no este repetido
+            if not general.nodo_en_lista_cerrado(self.lista_cerrado, nodo):
                 self.lista_cerrado.append(nodo)
                 sucesores = general.obtener_sucesores(nodo)
-                if len(sucesores) > 0 and not general.validar_puzzle_en_nodos(sucesores, self.fin):
+
+                print("Nodo")
+                general.imprimir_bonito(nodo.get_puzzle())  # esto es un print
+                print("")
+                print("Sucesores")
+                for i in sucesores:
+                    print(i.get_regla_aplicada())
+                    general.imprimir_bonito(i.get_puzzle())
+                    print("")
+
+
+                if len(sucesores) > 0 and not general.validar_puzzle_en_nodos(sucesores, self.fin.copy()):
                     for nod in sucesores:
+                        #if general.validar_puzzle_en_nodos(sucesores)
                         self.lista_abierto.append(nod)
-                if general.validar_puzzle_en_nodos(sucesores, self.fin):
+                        #print("metio uno a sucesores")
+                elif len(sucesores) == 0:
+                    print("No hay sucesores")
+                elif general.validar_puzzle_en_nodos(sucesores, self.fin.copy()):
                     self.nodo_final = general.validar_puzzle_en_nodos(sucesores, self.fin)
                     break
         if self.nodo_final:
-            messagebox.showinfo(title="Exito")
+            messagebox.showinfo(title="Exito", message="Exito, nodo encontrado")
             print(self.nodo_final.get_puzzle())
 
 lista1 = [
-    1, 2, 3, 4, 5, 6, 7, 0, 8
+    1, 2, 3, 4, 5, 6, 7, 8, 0
 ]
 
 lista2 = [
-    1, 2, 3, 4, 5, 6, 7, 8, 0
+    1, 2, 3, 4, 0, 6, 7, 8, 5
 ]
-#nodoP1 = nodo_class(None, lista1, "asd")
 
-#sucesores = general.obtener_sucesores(nodoP1)
-
-#for i in sucesores:
-#    general.imprimir_bonito(i.get_puzzle())
-#    print("")
-
-#response = general.validar_puzzle_en_nodos([nodoP1], lista1)
-
-#print(response)
-
-algoritmo_BPA(lista1, lista2)
+lista3 = [
+    6, 2, 5, 4, 3, 0, 1, 7, 8
+]
 
 
-#nodoP2 = nodo_class(None, lista1, "asd")
+nodoP1A = nodo_class(None, lista1.copy(), "asd")
+nodoP1B = nodo_class(None, lista1.copy(), "asd")
+
+nodoP2A = nodo_class(nodoP1A, lista2.copy(), "asd")
+nodoP2B = nodo_class(nodoP1B, lista2.copy(), "asd")
+
+nodoP3A = nodo_class(nodoP2A, lista3.copy(), "asd")
+nodoP3B = nodo_class(nodoP2B, lista3.copy(), "asd")
+
+nodoP4A = nodo_class(nodoP3A, lista2.copy(), "asd")
+nodoP4B = nodo_class(nodoP3B, lista2.copy(), "asd")
+
+#respose = general.validar_padres(nodoP4A, nodoP4B)
+#print(respose)
 
 
-#nodo1 = nodo_class(nodoP1, lista1, "fgh")
-#nodo2 = nodo_class(nodoP1, lista1, "fgh")
+algoritmo_BPA(lista1, lista3)
 
-#nodoP1.set_hijo(nodo1)
-
-
-
-#print(nodo1)
-#print(nodo2)
-
-#response = general.validar_nodos_iguales(nodo1, nodo2)
-#print(response)
--- general.py
+++ general.py
@@ -22,8 +22,16 @@ def validar_nodos_iguales(nodo1: nodo_class, nodo2: nodo_class):
         return False
 
 def nodo_en_lista(lista, nodo: nodo_class):
+    iguales = False
     for nod in lista:
-        if validar_nodos_iguales(nod, nodo):
+        if validar_padres(nod, nodo):
+            iguales = True
+
+    return iguales
+
+def nodo_en_lista_cerrado(lista, nodo: nodo_class):
+    for i in lista:
+        if i.get_puzzle() == nodo.get_puzzle():
             return True
     return False
 
@@ -32,6 +40,38 @@ def validar_puzzle_en_nodos(lista, puzzle):
         if nodo.get_puzzle() == puzzle:
             return nodo
     return False
+
+def validar_padres(nodo1: nodo_class, nodo2: nodo_class):
+    es_igual = False
+    if(nodo1.get_puzzle() == nodo2.get_puzzle()):
+        nodo_padre1 = nodo1.get_padre()
+        nodo_padre2 = nodo2.get_padre()
+        if nodo_padre1 and nodo_padre2:
+            while nodo_padre1 and nodo_padre2:
+                if nodo_padre1.get_puzzle() == nodo_padre2.get_puzzle():
+                    if nodo_padre1.get_padre() and nodo_padre2.get_padre():
+                        nodo_padre1 = nodo_padre1.get_padre()
+                        nodo_padre2 = nodo_padre2.get_padre()
+                        if nodo_padre1.get_puzzle() == nodo_padre2.get_puzzle():
+                            es_igual = True
+                        else:
+                            return False
+                    else:
+                        if nodo_padre1.get_padre() == None and nodo_padre2.get_padre() == None:
+                            return True
+                        else:
+                            return False
+                else:
+                    return False
+        else:
+            if nodo_padre1 == None and nodo_padre2 == None:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+    return es_igual
 
 def imprimir_bonito(puzzle):
     if puzzle:
@@ -46,55 +86,29 @@ def imprimir_bonito(puzzle):
 
 def obtener_sucesores(nodo: nodo_class):
     sucesores = list()
-    responses = list()
     for i in range(4):
-
         if i == 0:
             puzzle1 = reglas.mover_0_derecha(nodo.get_puzzle().copy())
-            #print(puzzle1)
             if puzzle1:
                 if validar_puzzle(puzzle1):
-                    responses.append(puzzle1)
-                    #print(i)
-                    #imprimir_bonito(puzzle)
                     nuevo_nodo = nodo_class(nodo, puzzle1, "mover 0 derecha")
                     sucesores.append(nuevo_nodo)
-                    #print(nuevo_nodo.get_puzzle())
         if i == 1:
             puzzle2 = reglas.mover_0_abajo(nodo.get_puzzle().copy())
-            #print(puzzle2)
             if puzzle2:
                 if validar_puzzle(puzzle2):
-                    responses.append(puzzle2)
-                    #print(i)
-                    #imprimir_bonito(puzzle)
                     nuevo_nodo = nodo_class(nodo, puzzle2, "mover 0 abajo")
                     sucesores.append(nuevo_nodo)
-                    #print(nuevo_nodo.get_puzzle())
         if i == 2:
             puzzle3 = reglas.mover_0_izquierda(nodo.get_puzzle().copy())
-            #print(puzzle3)
             if puzzle3:
                 if validar_puzzle(puzzle3):
-                    responses.append(puzzle3)
-                    #print(i)
-                    #imprimir_bonito(puzzle)
                     nuevo_nodo = nodo_class(nodo, puzzle3, "mover 0 izquierda")
                     sucesores.append(nuevo_nodo)
-                    #print(nuevo_nodo.get_puzzle())
         if i == 3:
             puzzle4 = reglas.mover_0_arriba(nodo.get_puzzle().copy())
-            #print(puzzle4, "Entro")
             if puzzle4:
                 if validar_puzzle(puzzle4):
-                    responses.append(puzzle4)
-                    #print(i)
-                    #imprimir_bonito(puzzle)
                     nuevo_nodo = nodo_class(nodo, puzzle4, "mover 0 arriba")
                     sucesores.append(nuevo_nodo)
-                    #print(nuevo_nodo.get_puzzle(), "Entro 2")
-
-    #print(responses)
-
-
     return sucesores
